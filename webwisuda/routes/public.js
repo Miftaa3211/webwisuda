93a402eec70e9410fbb825a833252455f25c8626
miftakhur rizky <159707649+Miftaa3211@users.noreply.github.com>
2025-12-03 21:19:38 +0700

    Delete webwisuda directory

--- webwisuda/routes/public.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const publicController = require('../controllers/publicController');
-
-router.get('/', publicController.index);
-
-module.exports = router;
